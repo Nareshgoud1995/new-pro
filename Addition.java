48a5d5a3dfe4574e32bd40c13279e611e49357be
sadhanavemula@gmail.com <sadhanavemula@gmail.com>
2021-05-31 14:41:42 +0530

    welcome pro

---
+++ Addition.java
@@ -0,0 +1,13 @@
+package com.anil;
+
+public class Addition {
+	
+	public static void add (int x,int y) {
+	int	res=x+y;
+	System.out.println("add is ,"+res);
+	}
+	
+public static void main(String[] args) {
+	Addition.add(55,20);
+}
+}
---
+++ Multiplication.java
@@ -0,0 +1,13 @@
+package com.anil;
+
+public class Multiplication {
+	private static void mul( int a,int b) {
+	int x=a*b;
+	
+	System.out.println("mul is "+x);
+		
+	}
+public static void main(String[] args) {
+	Multiplication.mul(25, 2);
+}
+}
